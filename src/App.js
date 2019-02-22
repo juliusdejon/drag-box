50de0ec3b17013f2cc950e6029fe23a5a0eeb7d6
Gelline Gruta <gelline.gruta@shoresuite.com>
2019-02-22 15:23:29 +0800

    ?

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
                 </td>
               </tr>
               <tr>
-                <td><DragContainer /></td>
+                <td><DragContainer items={second} /></td>
                 <td>  
                   <DragContainer items={third} />
                 </td>
